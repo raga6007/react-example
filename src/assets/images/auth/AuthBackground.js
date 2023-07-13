0e1c1c2893733843417adec07c344913f14f5b0b
Test <test@gmail.com>
2023-07-13 10:37:55 +0530

    init

---
+++ src/assets/images/auth/AuthBackground.js
@@ -0,0 +1,15 @@
+// material-ui
+
+import { Box } from '@mui/material';
+
+// ==============================|| AUTH BLUR BACK SVG ||============================== //
+
+const AuthBackground = () => {
+  return (
+    <Box sx={{ position: 'absolute', filter: 'blur(18px)', zIndex: -1, bottom: 0 }}>
+ 
+    </Box>
+  );
+};
+
+export default AuthBackground;
